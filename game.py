3a16e354503059038d1de3dd69d9c04c1c2e0d6c
omanaz <o.a.reinhart@wustl.edu>
2023-10-14 11:44:23 -0500

    game

---
+++ game.py
@@ -0,0 +1,18 @@
+import pygame
+
+class Game:
+    def __init__(self, screen):
+        self.screen = screen
+
+    def handle_events(self, event):
+        # Handle game-specific events
+
+    def update(self):
+        # Update game-specific logic
+
+    def draw(self):
+        # Draw the game-specific elements
+
+    def next_state(self):
+        # Determine if the state should change
+        return None
