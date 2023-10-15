debb9b16691d64180bf262c9aeee4544e90f1a85
omanaz <o.a.reinhart@wustl.edu>
2023-10-15 11:23:36 -0500

    dfgdf

--- instructions.py
+++ instructions.py
@@ -7,7 +7,7 @@ class Instructions:
         self.font = pygame.font.Font(None, 36)
         self.button_font = pygame.font.Font(None, 24)
         self.button_color = (255, 0, 0)
-        self.button_rect = pygame.Rect(screen.get_width() // 2 - 75, 450, 150, 50) 
+        self.button_rect = pygame.Rect(screen.get_width() // 2 - 75, 850, 150, 50) 
 
     def handle_events(self, event):
         if event.type == pygame.QUIT:
@@ -27,12 +27,25 @@ class Instructions:
         # Create a "Instructions" text surface
         instructions_text = self.font.render("Instructions", True, (255, 255, 255))  # White text
         instructions_rect = instructions_text.get_rect()
-        instructions_rect.center = (self.screen.get_width() // 2, 100)  # Position the text
+        instructions_rect.center = (self.screen.get_width() // 2, 80)  # Position the text
 
         # Create a "Lorem Ipsum" text surface
-        lorem_text = self.font.render("Lorem Ipsum", True, (255, 255, 255))  # White text
+        in_text ="""
+        You are trapped on the moon and have to survive for ten days until you’re rescued.\n
+        Grow and harvest as many plants as possible under the planting and watering constraints. \n 
+        Increase each plant’s chances of growing by adding water. Don’t wait too long to water each plant or they’ll die :( \n
+        \n
+        Controls: \n
+        \n
+        Up, Down, Left, and Right arrows: move\n
+        Enter: dig hole \n
+        I: See plant menu. Click to make plant selection.\n
+        Shift: Plant current plant selection, (must be in a dug hole)\n
+        W: water plant \n
+        """
+        lorem_text = self.font.render(in_text, True, (255, 255, 255))  # White text
         lorem_rect = lorem_text.get_rect()
-        lorem_rect.center = (self.screen.get_width() // 2, 200)  # Position the text
+        lorem_rect.center = (self.screen.get_width() // 2, 400)  # Position the text
 
         pygame.draw.rect(self.screen, self.button_color, self.button_rect)  # Green button
         button_text = self.button_font.render("Menu", True, (255, 255, 255))
