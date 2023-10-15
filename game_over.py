7ed2ec5ad16e918a9339730d9d43f61371172328
Kayleighcrow <70977173+Kayleighcrow@users.noreply.github.com>
2023-10-15 10:18:46 -0500

    twenyfour

--- game_over.py
+++ game_over.py
@@ -2,49 +2,61 @@ import pygame
 import pygame_gui
 from pygame_gui.elements import UIButton
 
-class Settings:
-    def __init__(self, screen):
+class GameOver:
+    def __init__(self, screen, points):
         self.screen = screen
-        self.ui_manager = pygame_gui.UIManager((1280, 720))
-        self.clock = pygame.time.Clock()
-        self.slider_value = 0.5  # Initial slider value
-
-        button_rect = pygame.Rect(100, 100, 100, 40)  # Define the button's position and size
-        self.button = UIButton(button_rect, 'Menu', self.ui_manager)  # Create the button
-
-        # Create a slider element
-        slider_rect = pygame.Rect(100, 200, 200, 20)
-        self.slider = pygame_gui.elements.UIHorizontalSlider(slider_rect, self.slider_value, (0.0, 1.0), self.ui_manager)
-
-
-        value_label_rect = pygame.Rect(100, 175, 200, 20)
-        self.value_label = pygame_gui.elements.UILabel(value_label_rect, str(self.slider_value), self.ui_manager)
-
+        self.points = points 
+        font_path = r'data\LycheeSoda.ttf'
+        self.font = pygame.font.Font(font_path, 50)
+        self.button_x = self.screen_width/2 - (self.button_width/2)
+        self.button_y = 100
+        self.button_width = 300
+        self.button_height = 75
+        self.button_color = (255, 0, 0)  # Red color for the button
+        self.button_list = ['New Game','Menu','Exit Game']
 
     def handle_events(self, event):
-        # Handle settings-specific events
-        if event.type == pygame.QUIT:
-            pygame.quit()
-        self.ui_manager.process_events(event)
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            if self.button.rect.collidepoint(event.pos):
-                return 'menu'
+        
+        # Handle menu-specific events
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:  # Check for left mouse button click
+                mouse_x, mouse_y = event.pos
+                for i, button_text in enumerate(self.button_list):
+                    button_rect = pygame.Rect(self.button_x, self.button_y + 100 * i, self.button_width, self.button_height)
+                    if button_rect.collidepoint(mouse_x, mouse_y):
+                        if button_text == 'New Game':
+                            return "game"  # Transition to the game state
+                        elif button_text == 'Menu':
+                            return "menu"  # Transition to the settings state
+                        elif button_text == 'Exit Game':
+                            pygame.quit()
+                            return None
+
 
     def update(self):
         # Update settings-specific logic
         self.ui_manager.update(self.clock.tick(60) / 1000.0)
 
-        # Get the current slider value
-        self.slider_value = self.slider.get_current_value()
-
-        # Update the value label with the current slider value
-        self.value_label.set_text(f"Volume: {self.slider_value*100:.1f}%")
-
+        
     def draw(self):
-        # Draw the settings-specific elements
+        # Draw the menu-specific elements
+        game_over_text = self.font.render("Game Over", True, white)
+
+        text_x = self.screen_width // 2 - game_over_text.get_width() // 2
+        text_y = self.screen_height // 2 - game_over_text.get_height() // 2
+
         self.screen.fill((0, 0, 128))  # Navy background color
         self.ui_manager.draw_ui(self.screen)
+        # Blit the "Game Over" text onto the screen
+        self.screen.blit(game_over_text, (text_x, text_y))
 
+        for i, button_text in enumerate(self.button_list):
+            button_rect = pygame.draw.rect(self.screen, self.button_color, (self.button_x, self.button_y + 100 * i, self.button_width, self.button_height))
+            text = self.font.render(button_text, True, (255, 255, 255))
+            self.screen.blit(text, (self.button_x + 25, self.button_y + 100 * i))
+
+
+        pygame.display.flip()
     def next_state(self):
         # Determine if the state should change
         return None
--- menu.py
+++ menu.py
@@ -7,7 +7,7 @@ class Menu:
         self.screen_width, self.screen_height = pygame.display.get_surface().get_size()
         font_path = r'data\LycheeSoda.ttf'
         self.font = pygame.font.Font(font_path, 36)
-        self.button_x = self.screen_width/2
+        self.button_x = self.screen_width/2 - (self.button_width/2)
         self.button_y = 100
         self.button_width = 300
         self.button_height = 75
